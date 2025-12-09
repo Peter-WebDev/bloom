f024fcf9e5efeb15dadd7ea405346d34b95ff501
Peter-WebDev <peter.warholm@gmail.com>
2025-12-09 01:17:19 +0100

    chore: update dependencies and remove unused migrations
    - Updated Next.js from 15.3.2 to 15.3.6.
    - Updated React and React DOM from 19.0.0 to 19.2.1.
    - Updated Prisma Client and Prisma from 6.8.2 to 7.1.0.
    - Added a new Prisma configuration file (prisma.config.ts).
    - Modified Prisma client initialization to include accelerateUrl.
    - Removed obsolete migration files related to user table schema changes.
    - Cleaned up the schema.prisma file by removing the DATABASE_URL from the datasource block.

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,4 +2,6 @@
 import { PrismaClient } from "../generated/prisma";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-export const db = new PrismaClient().$extends(withAccelerate());
+export const db = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL,
+}).$extends(withAccelerate());
